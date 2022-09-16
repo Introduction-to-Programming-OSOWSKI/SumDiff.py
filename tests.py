751b46ff853d37be627bec806f91f36c6304a7e4
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2022-09-16 08:17:21 -0500

    due date

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2021
-month = 12
-day = 30
+year = 2022
+month = 9
+day = 17
 
 def test_code():
     assert main.sumDiff(10, 5) == 10, "sumDiff(10, 5) == 10 failed"
